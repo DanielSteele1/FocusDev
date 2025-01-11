8c6a99b37b7415fb21ef93653c95f4929c40ae1d
Daniel Steele <Dsteele1906@gmail.com>
2025-01-11 17:04:44 +0000

    work on the dashboard UI

--- utilities-app/src/App.js
+++ utilities-app/src/App.js
@@ -9,33 +9,43 @@ import AddLinkIcon from '@mui/icons-material/AddLink';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import GithubIcon from '@mui/icons-material/GitHub';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import AddIcon from '@mui/icons-material/Add';
+import IconButton from '@mui/material/IconButton';
 
 import CloseIcon from '@mui/icons-material/Close';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 
 import NightsStayIcon from '@mui/icons-material/NightsStay';
 import LightModeIcon from '@mui/icons-material/LightMode';
 
+import { MantineProvider, Button, Text } from '@mantine/core';
+import { Calendar } from '@mantine/dates';
 
+import EmojiPicker from 'emoji-picker-react';
+
+import { TextField } from '@mui/material';
 
 function Navigation() {
 
   return (
     <div className="Navigation">
 
-      <div className="Navigation-Logo"> Logo </div>
-      <div className="Navigation-Logo"> <span>Application Name</span> </div>
-
+      <div className="Navigation-Logo"> <TrendingUpIcon sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '40px' }}>  </TrendingUpIcon> </div>
+      <div className="Navigation-Name"> <span> FocusDev </span> </div>
 
       <div className="Nav-Menu">
 
-        <div className="Navigation-Item"> <span> <NightsStayIcon> </NightsStayIcon> </span> </div>
-        <div className="Navigation-Item"> <span> <AccessTimeIcon> </AccessTimeIcon> Time and Date </span> </div>
-      </div> 
+        <div className="Navigation-Item"> <span className="darkMode"> <NightsStayIcon sx={{ justifyContent: 'center', alignItems: 'center', margintop: '10px' }}>  </NightsStayIcon> </span> </div>
+        <div className="Navigation-Item" id="time">
+          <span className="time">
+            <AccessTimeIcon sx={{ justifyContent: 'center', alignItems: 'center', margin: '10px' }} />
+            Time , GMT
+          </span>
+        </div>
+      </div>
     </div>
   );
-
 }
-
 
 function Dashboard() {
 
@@ -44,34 +54,63 @@ function Dashboard() {
 
       <div className="Dashboard">
 
+        <div className="Welcome"> <span> <span id="emoji">👋</span>  Welcome back, Daniel </span> </div>
+
         <div className="Dashboard-Top">
+
           <div className="Dashboard-Item" id="Useful-Links"> <span> <PushPinIcon sx={{ justifyContent: 'center', alignItems: 'center', marginRight: '5px' }}> </PushPinIcon>Useful Links </span> </div>
-          <div className="Dashboard-Item" id="Upcoming-Events"> <span> <CalendarMonthIcon sx={{ justifyContent: 'center', alignItems: 'center', marginRight: '5px' }}> </CalendarMonthIcon>Upcoming Events </span> </div>
-          <div className="Dashboard-Item" id="Weather"> <span> <CloudQueueIcon sx={{ justifyContent: 'center', alignItems: 'center', marginRight: '5px' }}> </CloudQueueIcon> Weather </span> </div>
           <div className="Dashboard-Item" id="Pinboard"> <span> <PushPinIcon sx={{ justifyContent: 'center', alignItems: 'center', marginRight: '5px' }}> </PushPinIcon> Pinboard </span> </div>
+          <div className="Dashboard-Item" id="Upcoming-Events"> <span> <CalendarMonthIcon sx={{ justifyContent: 'center', alignItems: 'center', marginRight: '5px' }}> </CalendarMonthIcon>Upcoming Events </span> </div>
+
+          <div className="Dashboard-Item" id="Weather"> <span> <CloudQueueIcon sx={{ justifyContent: 'center', alignItems: 'center', marginRight: '5px' }}> </CloudQueueIcon> Weather </span> </div>
 
         </div>
 
         <div className="Dashboard-Mid">
-          <div className="Dashboard-Item" id="Notes"> <span> <NotesIcon sx={{ justifyContent: 'center', alignItems: 'center', marginRight: '5px' }}> </NotesIcon> Notes </span> </div>
+          <div className="Dashboard-Item" id="Notes">
+            <span>
+              <NotesIcon sx={{ justifyContent: 'center', alignItems: 'center', marginRight: '5px' }}> </NotesIcon>
+              Notes
+            </span>
+            <div className="Notes-Controls">
+              <div className="Notes-input">
+                <input id="NotesInput" type="text" placeholder="Add a note">
+
+                </input>
+                <div id="Notes-Buttons">
+                  <IconButton color="green"> <AddIcon color="green">  </AddIcon>  </IconButton>
+                  <IconButton color="green"> <CloseIcon> </CloseIcon> </IconButton>
+                </div>
+
+              </div>
+            </div>
+            <div className="Notes-Content">
+            </div>
+            <Text className="Note"> This is a note </Text>
+            <Text className="Note"> This is a note </Text>
+            <Text className="Note"> This is a note </Text>
+            <Text className="Note"> This is a note </Text>
+            <Text className="Note"> This is a note </Text>
+          </div>
           <div className="Dashboard-Item" id="ToDo"> <span> <CalendarMonthIcon sx={{ justifyContent: 'center', alignItems: 'center', marginRight: '5px' }}> </CalendarMonthIcon> To Do </span> </div>
-          <div className="Dashboard-Item" id="Calender"> <span> <CalendarMonthIcon sx={{ justifyContent: 'center', alignItems: 'center', marginRight: '5px' }}> </CalendarMonthIcon> Calender </span> </div>
+          <div className="Dashboard-Item" id="Calender"> <span> <CalendarMonthIcon sx={{ justifyContent: 'center', alignItems: 'center', marginRight: '5px' }}>
+          </CalendarMonthIcon> Calendar <Calendar /> </span>
+
+          </div>
         </div>
 
         <div className="Dashboard-Bottom">
-        <div className="Dashboard-Item" id="Habit-Tracker"> <span> <CalendarMonthIcon sx={{ justifyContent: 'center', alignItems: 'center', marginRight: '5px' }}> </CalendarMonthIcon> Habit Tracker </span> </div>
+          <div className="Dashboard-Item" id="Habit-Tracker"> <span> <CalendarMonthIcon sx={{ justifyContent: 'center', alignItems: 'center', marginRight: '5px' }}> </CalendarMonthIcon> Habit Tracker </span> </div>
           <div className="Dashboard-Item" id="Calorie-Tracker"> <span>  <CalendarMonthIcon sx={{ justifyContent: 'center', alignItems: 'center', marginRight: '5px' }}> </CalendarMonthIcon> Calorie Tracker </span> </div>
+          <div className="Dashboard-Item" id="Github-Commit-Graph"> <span> <GithubIcon sx={{ justifyContent: 'center', alignItems: 'center', marginRight: '5px' }}> </GithubIcon> Github Commit Graph </span> </div>
 
-        <div className="Dashboard-Item" id="Github-Commit-Graph"> <span> <GithubIcon sx={{ justifyContent: 'center', alignItems: 'center', marginRight: '5px' }}> </GithubIcon> Github Commit Graph </span> </div>
         </div>
-
 
       </div>
     </div>
   );
 
 }
-
 
 function Footer() {
 
@@ -85,18 +124,15 @@ function Footer() {
 
 }
 
-
 function App() {
   return (
-    <div className="App">
-
-      <Navigation />
-
-      <Dashboard />
-
-      <Footer />
-
-    </div>
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: 'dark' }}>
+      <div className="App">
+        <Navigation />
+        <Dashboard />
+        <Footer />
+      </div>
+    </MantineProvider>
   );
 }
 
